42193437f573842481edd52eecd4c0a23b11c6ea
deanrobertcook <deanrobertcook@gmail.com>
2014-10-24 22:15:29 +0800

    Refactored and included html beautifier

--- source/Controller.js
+++ source/Controller.js
@@ -1,68 +1,77 @@
 $(document).ready(function() {
-	controller = new Controller();
+	new Controller();
 });
 
 function Controller() {
 	this.model = new Model();
 	this.view = new View();
+	//add post input fields
+	this.view.producePostInformationTextField("Title", {"id": "title", "name": "title", "value": "Test"});
+	this.view.producePostInformationTextField("Author", {"id": "author", "name": "author", "value": "the Ronin"});
+	this.view.producePostInformationTextField("Date", {"id": "title", "name": "title", "value": "test"});
 	
-	this.$pageHook = this.findPageHook();	
-	this.view.produceGUI(this.$pageHook);
-	this.setup();
-	this.attachHandlers();
+	//add toolbar buttons
+	this.view.produceToolbarButton("OL", this.orderedList.bind(this));
+	this.view.produceToolbarButton("UL", this.unorderedList.bind(this));
+	this.view.produceToolbarButton("Link", this.createLink.bind(this));
+	this.view.produceToolbarButton("TidyHTML", this.updatePlainTextArea.bind(this));
+	this.view.produceToolbarButton("Submit", this.submitPost.bind(this));
+	
+	this.view.assembleGUI();
+	
+	this.iframeDoc = this.view.richTextArea.get(0).contentWindow.document;
+	this.iframeDoc.designMode = 'On';
+	
+	this.view.plainTextArea.on("input", this.updateRichTextArea.bind(this));
+	$(this.iframeDoc).on("input", this.updatePlainTextArea.bind(this));
 };
 
-Controller.prototype = {	
-	findPageHook: function() {
-		return $("#textEditor");
-	},
-	
-	setup: function() {
-		richTextContent.document.designMode = 'On';
-	},
-	
+Controller.prototype = {
 	updatePlainTextArea: function() {
-		$("#plainTextContent").val(this.extractRichText());
+		var html = this.extractRichText();
+		html = html_beautify(html, {
+			'indent_inner_html': false,
+			'indent_size': 2,
+			'indent_char': ' ',
+			'wrap_line_length': 78,
+			'brace_style': 'expand',
+			'unformatted': ['a', 'sub', 'sup', 'b', 'i', 'u'],
+			'preserve_newlines': true,
+			'max_preserve_newlines': 5,
+			'indent_handlebars': false
+		  });
+		this.view.plainTextArea.val(html);
+	},
+	
+	extractRichText: function() {
+		return $(this.iframeDoc.body).html();
 	},
 	
 	updateRichTextArea: function() {
-		$(richTextContent.document.body).html(this.extractPlainText());
+		$(this.iframeDoc.body).html(this.extractPlainText());
+	},
+	
+	extractPlainText: function() {
+		return this.view.plainTextArea.val();
 	},
 	
 	orderedList: function() {
-		richTextContent.document.execCommand("insertOrderedList");
-		$("#richTextContent").focus();
+		this.iframeDoc.execCommand("insertOrderedList");
+		$(this.view.richTextArea).focus();
 	},
 	
 	unorderedList: function() {
-		richTextContent.document.execCommand("insertUnorderedList");
-		$("#richTextContent").focus();
+		this.iframeDoc.execCommand("insertUnorderedList");
+		$(this.view.richTextArea).focus();
 	},
 	
 	createLink: function() {
 		var link = prompt("Enter link: ", "http://");
-		richTextContent.document.execCommand("createLink", false, link);
-		$("#richTextContent").focus();
-	},
-	
-	attachHandlers: function() {
-		$("#submit").click(this.submitPost.bind(this));
-		$("#orderedListButton").click(this.orderedList);
-		$("#unorderedListButton").click(this.unorderedList);
-		$("#createLinkButton").click(this.createLink);
-		$(richTextContent.document.body).keyup(this.updatePlainTextArea.bind(this));
-		$("#plainTextContent").keyup(this.updateRichTextArea.bind(this));
+		this.iframeDoc.execCommand("createLink", false, link);
+		$(this.view.richTextArea).focus();
 	},
 	
 	submitPost: function() {
-		console.log(this.extractRichText());
-	},
-	
-	extractPlainText: function() {
-		return $("#plainTextContent").val();
-	},
-	
-	extractRichText: function() {
-		return $(richTextContent.document.body).html();
+		console.log(this.extractPlainText());
 	},
 };
--- source/View.js
+++ source/View.js
@@ -1,62 +1,56 @@
 function View() {
-	this.fieldHooks = [];
+	this.postInformationDiv = this.produceSimpleDiv("postInfo");
+	this.plainTextArea = this.producePlainTextArea();
+	this.toolBar = this.produceSimpleDiv("toolBar");
+	this.richTextArea = this.produceRichTextArea();
 };
 
 View.prototype = {
-	produceGUI: function($pageHook) {
-		$pageHook.append(this.produceInputTextField("title", "Title: "));
-		$pageHook.append(this.produceInputTextField("author", "Author: "));
-		$pageHook.append(this.produceTextArea());
-		$pageHook.append(this.produceToolBar());
-		$pageHook.append(this.produceIFrame());
-		$pageHook.append(this.produceSubmitButton());
+	assembleGUI: function() {
+		var mainDiv = $("#textEditor");
+		mainDiv.append(this.postInformationDiv);
+		mainDiv.append(this.plainTextArea);
+		mainDiv.append(this.toolBar);
+		mainDiv.append(this.richTextArea);
+		mainDiv.append(this.produceFooter());
 	},
 	
-	produceInputTextField: function(id, label, value) {
-		value = value || "";
-		this.fieldHooks.push("#" + id);
+	produceSimpleDiv: function(id) {
+		var divElement = $("<div id='"+id+"'></div>");
+		return divElement;
+	},
+	
+	producePostInformationTextField: function(label, inputAttributes) {
+		var divElement = $("<div></div>");
+		var labelElement = $("<label for='"+inputAttributes.id+"'>"+label+": </label>");
+		var inputElement = $("<input>");
+		inputElement.attr(inputAttributes);
 		
-		var html = [
-			'<div>',
-				'<label for="'+id+'">'+label+'</label>',
-				'<input id="'+id+'" name="'+id+'" type="text" value="'+value+'">',
-			'</div>'
-		].join('\n');
-		
-		return html;
+		divElement.append(labelElement);
+		divElement.append(inputElement);
+		this.postInformationDiv.append(divElement);
 	},
 	
-	produceToolBar: function() {
-		var html = [
-			'<div>',
-				'<button id="orderedListButton">OL</button>',
-				'<button id="unorderedListButton">UL</button>',
-				'<button id="createLinkButton">Link</button>',
-			'</div>'
-		].join('\n'); 
-		return html;
+	producePlainTextArea: function(value) {
+		var textAreaElement = $("<textarea id='plainTextArea' name='plainTextArea' style='width: 700px; height: 300px;'></textarea>");
+		return textAreaElement;
 	},
 	
-	produceTextArea: function(value) {
-		value = value || "";
-		var id = "plainTextContent";
-		this.fieldHooks.push("#" + id);
-		return	"<textarea id='"+id+"' name='"+id+"' style='width: 700px; height: 300px;'>" +
-				"</textarea>";
+	produceToolbarButton: function(label, handler) {
+		var button = $("<button>"+label+"</button>");
+		button.click(handler);
+		$(this.toolBar).append(button);
 	},
 	
-	produceIFrame: function() {
-		var id = "richTextContent";
-		this.fieldHooks.push("#content");
-		return	"<iframe id='"+id+"' name='"+id+"' style='border: 1px solid black; width: 700px; height: 300px;'></iframe>";
+	produceRichTextArea: function() {
+		var iFrameElement = $("<iframe id='richTextContent' style='border: 1px solid black; width: 700px; height: 300px;'></iframe>");
+		return iFrameElement;
 	},
 	
-	produceSubmitButton: function() {
-		var id = "submit";
-		this.fieldHooks.push("#content");
-		var html = [
-				'<input id="'+id+'" name="'+id+'" type="button" value="Submit Data">'
-		].join('\n');
-		return html;
+	produceFooter: function() {
+		var paragraph = $("<p>Thanks to Nochum Sossonko and the JS Beautifier team for the html beautifier. <br>\n\
+							The source code for that is available <a href='https://github.com/beautify-web/js-beautify'>here</a><br>\n\
+							(double thanks, for teaching me some new Javscript syntax!)</p>");
+		return paragraph;
 	}
 };
